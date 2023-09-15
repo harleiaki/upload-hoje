26a07d6cb393b7676fd339b4bc051b7086b45efd
harlei akira <harlei.akira@gmail.com>
2023-09-15 14:08:33 -0300

    ok

--- ai/src/components/ui/button.tsx
+++ ai/src/components/ui/button.tsx
@@ -4,6 +4,8 @@ import { cva, type VariantProps } from "class-variance-authority"
 
 import { cn } from "@/lib/utils"
 
+
+
 const buttonVariants = cva(
   "inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50",
   {
@@ -21,10 +23,10 @@ const buttonVariants = cva(
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
-        default: "h-9 px-4 py-2",
-        sm: "h-8 rounded-md px-3 text-xs",
-        lg: "h-10 rounded-md px-8",
-        icon: "h-9 w-9",
+        default: "h-7 px-4 py-2",
+        sm: "h-6 rounded-md px-3 text-xs",
+        lg: "h-8 rounded-md px-6",
+        icon: "h-7 w-7",
       },
     },
     defaultVariants: {
@@ -40,6 +42,7 @@ export interface ButtonProps
   asChild?: boolean
 }
 
+
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, variant, size, asChild = false, ...props }, ref) => {
     const Comp = asChild ? Slot : "button"
@@ -53,5 +56,7 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   }
 )
 Button.displayName = "Button"
+
+
 
 export { Button, buttonVariants }
